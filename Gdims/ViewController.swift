66117dd3725998f448bb75896e1a99762e159853
qingsong <1154046205@qq.com>
2017-10-13 11:19:03 +0800

    原始1

--- Gdims/ViewController.swift
+++ Gdims/ViewController.swift
@@ -15,5 +15,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
