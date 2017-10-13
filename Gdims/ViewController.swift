0e2b43c03f269699a77d2f5a202bf389cbce8ca2
qingsong <1154046205@qq.com>
2017-10-13 11:16:11 +0800

    原始

--- Gdims/ViewController.swift
+++ Gdims/ViewController.swift
@@ -9,22 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    git init
-    git add .
-    git commit -m "first commit"
-    git remote add origin https://github.com/SonGuoer/Gdims.git
-    git push -u origin master
+    
 }
 
