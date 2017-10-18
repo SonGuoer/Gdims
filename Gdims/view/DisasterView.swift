6020958b5a43666dc56760901c34caddc537375f
baohongyan <657175417@qq.com>
2017-10-18 08:57:04 +0800

    灾害点主页2

--- Gdims/view/DisasterView.swift
+++ Gdims/view/DisasterView.swift
@@ -20,19 +20,11 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
     let Sheight = UIScreen.main.bounds.size.height
     var clickNum:Int?
     var getClickNum:Int?
-    
-    //灾害点名称
-    var disasterNames = [String]()
-    //灾害点编号
-    var disasterNums = [String]()
-    //监测点名称
-    var monitorNames = [String]()
-    
+    var array = [String]()
     var url = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        macroRequest()
         self.myTableView = UITableView()
         self.myTableView!.frame = CGRect(x: 0, y: 80, width: Swidth, height: Sheight-20)
         self.myTableView!.delegate = self
@@ -40,29 +32,67 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
         self.myTableView!.tableFooterView = UIView()
         self.myTableView!.separatorStyle = .none
         self.view.addSubview(self.myTableView!)
-//        monitorRequst()
+        //        macroRequest()
+        monitorRequst()
+    }
+    
+    private func saveName(text: String) {
+        //        步骤一：获取总代理和托管对象总管
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedObectContext = appDelegate.persistentContainer.viewContext
+        
+        //        步骤二：建立一个entity
+        let entity = NSEntityDescription.entity(forEntityName: "Monitor", in: managedObectContext)
+        
+        let person = NSManagedObject(entity: entity!, insertInto: managedObectContext)
+        
+        //        步骤三：保存文本框中的值到person
+        person.setValue(text, forKey: "monPointName")
+        
+        //        步骤四：保存entity到托管对象中。如果保存失败，进行处理
+        do {
+            try managedObectContext.save()
+            print("保存成功")
+        } catch  {
+            fatalError("无法保存")
+        }
+        
+        //        步骤二：建立一个获取的请求
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Monitor")
+        
+        //        步骤三：执行请求
+        do {
+            let fetchedResults = try managedObectContext.fetch(fetchRequest) as? [Monitor]
+            print(fetchedResults)
+            for one in fetchedResults! {
+                print(one.monPointName)
+            }
+        } catch  {
+            fatalError("获取失败")
+        }
     }
     
     /*
      灾害点请求
      */
-    func macroRequest() {
+    func macroRequst()  {
         url = "http://183.230.108.112:8099/meteor/findMacro.do?mobile=15702310784&&imei=0"
         Alamofire.request(url).responseObject { (response: DataResponse<BaseModel>) in
             let myResponse = response.result.value
-            print(myResponse!.info!)
+            //            print(myResponse!.info!)
             let extractedExpr: [MacroInfoModel]? = Mapper<MacroInfoModel>().mapArray(JSONString: (myResponse?.info)!)
-            //let realm = try! Realm()
-            //try! realm.write {
+            
             for forecast in extractedExpr! {
                 //realm.create(InfoRealm.self, value: forecast, update: true)
-                self.disasterNames += [forecast.name!]
+//                self.saveName(text: forecast.name!)
             }
+            
             //}
             //print("数据库路径: \(realm.configuration.fileURL)")
         }
-        
     }
+    
     /*
      灾害点的监测点请求
      */
@@ -70,29 +100,28 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
         url = "http://183.230.108.112:8099/meteor/findMonitor.do?mobile=15702310784&&imei=0"
         Alamofire.request(url).responseObject { (response: DataResponse<BaseModel>) in
             let myResponse = response.result.value
-//            print(myResponse!.info!)
+            //            print(myResponse!.info!)
             let extractedExpr: [InfoModel]? = Mapper<InfoModel>().mapArray(JSONString: (myResponse?.info)!)
             
             for forecast in extractedExpr! {
                 //realm.create(InfoRealm.self, value: forecast, update: true)
                 self.saveName(text: forecast.monPointName!)
             }
-        
+            
             //}
             //print("数据库路径: \(realm.configuration.fileURL)")
         }
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
-//        print(self.disasterNames.count)
         return 2
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let numOfCells = [5,2]
         if clickNum == nil{
@@ -105,11 +134,7 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
             }
         }
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
-    }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let str = "section"
         var cell = self.myTableView?.dequeueReusableCell(withIdentifier: str)
@@ -117,12 +142,13 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
             cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: str)
         }
         cell?.textLabel?.text = "这是第\(indexPath.section+1)个字段,第\(indexPath.row)个cell"
-        cell?.frame.origin.x = 50
-        //设置分隔线
+    
+        //设置
         self.myTableView?.autoAddLineToCell(cell!, indexPath: indexPath, lineColor: UIColor(red: 232/255, green: 232/255, blue: 232/255, alpha: 1))
+        
         return cell!
     }
-    
+
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let view = UIView(frame: CGRect(x: 0, y: 0, width: Swidth-20, height: 60))
         view.backgroundColor = UIColor.white
@@ -132,8 +158,7 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
         title.tag = section
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapGesture(tap:)))
         title.addGestureRecognizer(tap)
-//        print(self.disasterNames[section])
-        title.text = "这是第\(section+1)个字段"
+        title.text = "这是第\(section+1)个段"
         title.textColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
         //设置图标
         let icon = UIImageView(frame: CGRect(x: view.frame.size.width-12, y: 22, width: 10, height: 18))
@@ -146,11 +171,11 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
         view.addSubview(line)
         return view
     }
-    
+
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 60
     }
-    
+
     @objc func tapGesture(tap:UITapGestureRecognizer){
         clickNum = tap.view?.tag
         if getClickNum == clickNum{
@@ -158,9 +183,8 @@ class DisasterView: UIViewController,UITableViewDelegate,UITableViewDataSource {
         }
         myTableView!.reloadData()
         getClickNum = clickNum
-//        tap.view?.backgroundColor = UIColor(red: 50/255, green: 187/255, blue: 170/255, alpha: 1)
     }
-    
+
 }
 
 extension UITableView {
@@ -181,7 +205,7 @@ extension UITableView {
     }
     
     private func addLineToCell(_ cell: UITableViewCell, lineColor: UIColor){
-        let view = UIView(frame: CGRect(x: 18, y: 39, width: self.bounds.width-38, height: 0.5))
+        let view = UIView(frame: CGRect(x: 18, y: 0, width: self.bounds.width-38, height: 0.5))
         view.tag = FLAG_TABLE_VIEW_CELL_LINE
         view.backgroundColor = lineColor
         cell.contentView.addSubview(view)
@@ -199,3 +223,4 @@ extension UITableView {
         return self.numberOfRows(inSection: atSection)
     }
 }
+
